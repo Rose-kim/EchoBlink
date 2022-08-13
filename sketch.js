cdd327b2f494736641224c0803f085445f3d42ae
Rose Kim <38057625+Rose-kim@users.noreply.github.com>
2022-08-13 13:19:47 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,12 +3,15 @@ let capture;
 function setup() {
   createCanvas(1280, 800);
   capture = createCapture(VIDEO);
-  capture.size(1280, 800);
+  capture.size(1280, 900);
   capture.hide();
+  img = loadImage('assets/visual_cue.png');
 }
 
 function draw() {
   background(255);
-  image(capture, 0, 0, 1280, 800);
+  image(capture, 0, 0,1280,900);
+  image(img,0,0);
+  //background(255);
   //filter(INVERT);
 }
